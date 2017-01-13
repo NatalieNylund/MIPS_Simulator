9e72238f6aab0b3106a92e68e912bfca97e1f2a8
Natalie Nylund <Natalie Nylund>
2017-01-13 14:26:39 +0100

    Added changes apperently

--- Simulator/src/PC.java
+++ Simulator/src/PC.java
@@ -4,8 +4,23 @@
 
 
 public class PC {
+	
+	private int value;
 
-	PC(){
-		
+	public PC(){
+		value = 0;
+	}
+	public void set(int value) {
+		assert value%4 == 0;
+		this.value = value;
+	}
+
+	public int get() {
+		return value;
+	}
+
+	public void reset() {
+		value = 0;
 	}
 }
+
