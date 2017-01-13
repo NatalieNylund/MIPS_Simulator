815d484c54c913a2700b53de7ae96b4b5268f53d
Natalie Nylund <Natalie Nylund>
2017-01-13 14:25:30 +0100

    Made changes

--- Simulator/src/InstructionMemory.java
+++ Simulator/src/InstructionMemory.java
@@ -1,7 +1,15 @@
-
+/*
+ * Takes instruction in mnemoic format and return binary format to control
+ */
 public class InstructionMemory {
-
-	InstructionMemory(){
+	private int op;
+	
+	public InstructionMemory(){
 		
 	}
+	/*
+	 * Transforms the mnempoic representation of instruction to binary and returns
+	 * the binary representation
+	 */
+
 }
