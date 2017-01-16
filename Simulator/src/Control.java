a090b324c7c2bad6672a0c4062f8f1829de88801
c14jsn <c14jsn@cs.umu.se>
2017-01-16 13:55:38 +0100

    added file

--- Simulator/src/Control.java
+++ Simulator/src/Control.java
@@ -5,90 +5,151 @@
 
 public class Control {
 
-	private boolean RegDist;
+	private boolean RegDst;
 	private boolean Branch;
 	private boolean MemRead;
 	private boolean MemtoReg;
 	private boolean ALUOp1;
 	private boolean ALUOp0;
 	private boolean MemWrite;
-	private boolean ALUsrc;
+	private boolean ALUSrc;
 	private boolean RegWrite;
+	private boolean Jump;
 	
-	public Control(){
+	public Control(Instruction instr){
+		setControlLines(instr);
+	}
+	
+	private void setControlLines(Instruction instr) {
+		setRegDst(false); 
+		setBranch(false);  
+		setMemRead(false);  
+		setMemtoReg(false);  
+		setALUOp1(false);  
+		setALUOp0(false);  
+		setMemWrite(false);  
+		setALUSrc(false);  
+		setRegWrite(false);  
 		
+		/* If R format */
+		if(instr.getType() == 'r') {
+			setRegDst(true);
+			setRegWrite(true);
+			setALUOp1(true);
+		}
+		/* If I format */
+		else if(instr.getType() == 'i') {
+			setRegWrite(true);
+			setALUSrc(true);
+			//ALUOp
+		}
+		/* If J format */
+		else if(instr.getType() == 'j') {
+			setJump(true);
+			//ALUOp?
+		}
+		/* If lw */
+		else if(instr.getOpcode() == 0x23) {
+			setALUSrc(true);
+			setMemtoReg(true);
+			setRegWrite(true);
+			setMemRead(true);
+		}
+		/* If sw */
+		else if(instr.getOpcode() == 0x2b) {
+			setALUSrc(true);
+			setMemWrite(true);
+		}
+		/* If beq */
+		else if(instr.getOpcode() == 0x04){
+			setBranch(true);
+			setALUOp0(true);
+		}
+
+	}
+	
+	public void update(Instruction instr) {
+		setControlLines(instr);
 	}
 
-	private void setRegDst(){
-		
+	private void setRegDst(boolean val){
+		RegDst = val;
 	}
 	
 	public Boolean getRegDst(){
-		
-		return true;
+		return RegDst;
 	}
 	
-	private void setALUSrc(){
-		
+	private void setALUSrc(boolean val){
+		ALUSrc = val;
 	}
 	
 	public Boolean getALUSrc(){
-		
-		return true;
+		return ALUSrc;
 	}
 	
-	private void setMemtoReg(){
-		
+	private void setMemtoReg(boolean val){
+		MemtoReg = val;
 	}
 	
 	public Boolean getMemtoReg(){
-		
-		return true;
+		return MemtoReg;
 	}
 	
-	private void setRegWrite(){
-		
+	private void setRegWrite(boolean val){
+		RegWrite = val;
 	}
 	
 	public Boolean getRegWrite(){
-		
-		return true;
+		return RegWrite;
 	}
 	
-	private void setMemRead(){
-		
+	private void setMemRead(boolean val){
+		MemRead = val;
 	}
 	
 	public Boolean getMemRead(){
-		
-		return true;
+		return MemRead;
 	}
 	
-	private void setMemWrite(){
-		
+	private void setMemWrite(boolean val){
+		MemWrite = val;
 	}
 	
 	public Boolean getMemWrite(){
-		
-		return true;
+		return MemWrite;
 	}
 	
-	private void setBranch(){
-		
+	private void setBranch(boolean val){
+		Branch = val;
 	}
 	
 	public Boolean getBranch(){
-		
-		return true;
+		return Branch;
 	}
 	
-	private void setALUop(){
-		
+	private void setALUOp1(boolean val){
+		ALUOp1 = val;
 	}
 	
-	public Boolean getALUop(){
-		
-		return true;
+	public Boolean getALUOp1(){
+		return ALUOp1;
+	}
+	
+	private void setALUOp0(boolean val){
+		ALUOp0 = val;
+	}
+	
+	public Boolean getALUOp0(){
+		return ALUOp0;
+	}
+	
+	private void setJump(boolean val){
+		Jump = val;
+	}
+	
+	public Boolean getJump(){
+		return Jump;
 	}
 	
 	
