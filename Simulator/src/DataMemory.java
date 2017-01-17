aa170e890358f34a5e40d4e4fb3ae223f3da5ca4
c14jsn <c14jsn@cs.umu.se>
2017-01-17 12:13:21 +0100

    added to DataMemory

--- Simulator/src/DataMemory.java
+++ Simulator/src/DataMemory.java
@@ -1,7 +1,31 @@
 
 public class DataMemory {
+	private byte[] data;
 
-	DataMemory(){
-		
+	public DataMemory(){
+		data = new byte[1000];
+		reset();
 	}
+
+	public void reset() {
+	    for(int i = 0; i < data.length; i++) {
+	        data[i] = 0;
+        }
+    }
+
+    public byte readMemory(int addr) {
+	    return get(addr);
+    }
+
+    public void writeMemory(int addr, byte data) {
+	    set(addr, data);
+    }
+
+	private byte get(int addr) {
+	    return data[addr];
+    }
+
+    private void set(int addr, byte val) {
+	    data[addr] = val;
+    }
 }
