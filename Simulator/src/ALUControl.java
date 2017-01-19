a7d42743fe54ce516ea6838cc398cde72f49f5bd
c14jsn <c14jsn@cs.umu.se>
2017-01-19 14:58:44 +0100

    modified

--- Simulator/src/ALUControl.java
+++ Simulator/src/ALUControl.java
@@ -9,6 +9,8 @@ public class ALUControl {
 	private short OR = 0x25;
 	private short NOR = 0x27;
 	private short SLT = 0x2a;
+	private short SRL = 0x02;
+	private short SRA = 0x03;
 	
 	private short ALU_AND = 0x0;
 	private short ALU_OR = 0x1;
@@ -16,6 +18,8 @@ public class ALUControl {
 	private short ALU_SUB = 0x6;
 	private short ALU_SLT = 0x7;
 	private short ALU_NOR = 0xc;
+	private short ALU_SRL = 0x3;
+	private short ALU_SRA = 0x4;
 			
 	
 	public ALUControl(){
@@ -33,7 +37,7 @@ public class ALUControl {
 		//For beq
 		else if(OpCode1 == 0 && OpCode0 == 1){
 			return ALU_SUB;
-		}else if(OpCode1 == 1){
+		}else if(OpCode1 == 1 && OpCode0 == 0){
 			
 			if(funct == ADD){
 				return ALU_ADD;
@@ -41,14 +45,22 @@ public class ALUControl {
 				return ALU_SUB;
 			}else if(funct == AND){
 				return ALU_AND;
-			}else if(funct == OR){
+			}else if(funct == OR) {
 				return ALU_OR;
 			}else if(funct == NOR){
 				return ALU_NOR;
-			}else if(funct == SLT){
+			}else if(funct == SLT) {
 				return ALU_SLT;
-			}else return -1;		
-		}else return -1;
+			} else if(funct == SRL) {
+				return ALU_SRL;
+			} else if (funct == SRA) {
+				return ALU_SRA;
+			}else return -1;
+		//For ori
+		}else if(OpCode0 == 1 && OpCode1 == 1) {
+			return ALU_OR;
+		}
+		else return -1;
 	}
 
 }
