2f1a08bbc5090cc6466bdd9a73b35f3be00e3daf
Natalie Nylund <oi11nnd@cs.umu.se>
2017-01-25 14:46:00 +0100

    Added comments

--- Simulator/src/ADD.java
+++ Simulator/src/ADD.java
@@ -1,5 +1,11 @@
 /*
- * Used to increment the PC to the address of the next instruction
+ * Course: Computer Organization and Architecture, HT16
+ * Author: Natalie Nylund (oi11nnd), Josefin Svensson (c14jsn)
+ * Date: 2017-01-25
+ * 
+ * Description:
+ * This class is used to increment the PC to the address of 
+ * the next instruction
  */
 
 public class ADD {
